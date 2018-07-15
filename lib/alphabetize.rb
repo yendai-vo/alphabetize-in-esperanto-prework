ac8054edc3668aed29e680c2d471360b91a4120c
Cindy Vo <yendai.vo@hotmail.com>
2018-07-15 06:32:12 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,12 +1,12 @@
 def alphabetize(arr)
   puts arr
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  # sortedArray = arr.sort_by do |phrase|
-  #   phrase.split("").map do |character|
-  #     return alphabet.index(character)
-  #   end
-  # end
-  sortedArray = arr.sort_by { |a| a.split("").map{ |char| alphabet.index(char) } }
+  sortedArray = arr.sort_by do |phrase|
+    phrase.split("").map do |char|
+      return alphabet.index(char)
+    end
+  end
+  # sortedArray = arr.sort_by { |a| a.split("").map{ |char| alphabet.index(char) } }
   puts 'woo'
   puts sortedArray
   sortedArray
