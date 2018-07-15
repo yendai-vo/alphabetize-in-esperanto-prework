77b43e8166aba12df9eb925c08df1d113d262161
Cindy Vo <yendai.vo@hotmail.com>
2018-07-15 06:25:58 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   alphabet = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |phrase|
-    phrase.split.map do |character|
+    phrase.split("").map do |character|
       return alphabet.index(character)
     end
   end
